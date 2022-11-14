b832b78ea1dbb3736ba29e30da129aaba8d1956e
sorinas96 <sorina.sateanu.maria@gmail.com>
2022-11-14 15:40:27 +0200

    update

--- Homework/functions_tema08.py
+++ Homework/functions_tema08.py
@@ -14,13 +14,16 @@ print(is_prime(3))
 #2.Scrieti o functie care are 2 parametrii nr naturale a,b cu a<b.
 # Functia returneaza lista numerelor prime din intervalul [a,b].
 
-def prime_numbers (a,b)
-  print("Prime numbers between", lower, "and", upper, "are:")
-  for num in range(lower, upper + 1):
+def list_of_primes_in_interval (a,b):
+  new_lst = []
+  #print("Prime numbers between", lower, "and", upper, "are:")
+  for x in range(a,b + 1):
     # all prime numbers are greater than 1
-    if num > 1:
-      for i in range(2, num):
-        if (num % i) == 0:
+    if x > 1:
+      for i in range(2, x):
+        if (x % i) == 0:
           break
       else:
-        print(num)
+        print(x)
+        new_lst.append(x)
+        return new_lst
--- Homework/test_functions_tema08.py
+++ Homework/test_functions_tema08.py
@@ -1,7 +1,7 @@
 #b) Scrieti 2 teste unitare (unul pentru True si unul pentru False) pentru functia is_prime
 
 import unittest
-from functions_tema08 import is_prime
+from functions_tema08 import is_prime,list_of_primes_in_interval
 class FunctionsTestCase(unittest.TestCase):
     def test_is_prime_01(self):
         expected_result = True
@@ -13,5 +13,22 @@ class FunctionsTestCase(unittest.TestCase):
         actual_result = True or False
         self.assertIsInstance(actual_result, int)
         self.assertEqual(expected_result, actual_result)
+
+    def test_list_of_primes_in_interval_01(self):
+        test_int = (9,1)
+        expected_result = [1,2,3,5,7,]
+
+        actual_result = prime_numbers(9,1)
+        self.assertIsInstance(actual_result, list)
+        self.assertEqual(actual_result, expected_result)
+
+    def test_list_of_primes_in_interval_02(self):
+        test_int = (11, 1)
+        expected_result = [1, 2, 3, 5, 7,11 ]
+
+        actual_result = prime_numbers(11, 1)
+        self.assertIsInstance(actual_result, list)
+        self.assertEqual(actual_result, expected_result)
+
 if __name__ == '__main__':
     unittest.main()
